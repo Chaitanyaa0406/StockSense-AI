47554c91bc71a1235dbcfd976763a80a60d90626
Chaitanyaa Pawar <pchaitanyaa@gmail.com>
2026-04-27 21:29:59 +0530

    Enable CORS

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const getAnalysis = async (symbol) => {
   const res = await axios.get(`http://127.0.0.1:8000/analyze/${symbol}`);
+  const API_URL = "https://your-backend.onrender.com";
   return res.data;
 };
